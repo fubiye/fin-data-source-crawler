ad4e4707e8a28d305dddf42a3e700e8fb5298fe3
Fu, Biye <biye.fu@outlook.com>
2019-06-27 22:18:20 +0800

    first commit

---
+++ src/main/java/com/ft/data/crawler/DataSourceCrawlerApplication.java
@@ -0,0 +1,13 @@
+package com.ft.data.crawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataSourceCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataSourceCrawlerApplication.class, args);
+	}
+
+}
